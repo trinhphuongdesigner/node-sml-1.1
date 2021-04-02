7f438fe57ca7e0ffc0bbcff1dbc201ee3173e959
Trinhphuong <phuong.trinh@orientsoftware.net>
2021-04-02 09:39:39 +0700

    update new source code

---
+++ README.txt
@@ -0,0 +1,10 @@
+KHỞI TẠO TỰ ÁN VỚI TÊN: NODE-SML
+KHỞI TẠO GIT: git init
+KHỞI TẠO NPM: npm init
+CÀI ĐẶT EXPRESS: npm i express --save
+TẠO 2 ROUTER "/" VÀ "/post"
+CÀI ĐẶT EJS (SET): view engine, views
+CÀI ĐẶT EJS-MATE: npm i ejs-mate, set public folder
+CHIA LAYOUT EJS: page, layout
+TRẢ DỮ LIỆU BÀI VIẾT VỀ CÁC PAGE 
+CẤU TRÚC LẠI ROUTER: express.Router() =>exports
--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const ejs = require("ejs");
 const ejsMate = require("ejs-mate");
 
-const routes = require("./routes");
-
-const port = 3000;
-
 const app = express();
+
+const routes = require("./routers");
+const port = 3001;
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.engine("ejs", ejsMate);
-app.use("/public", express.static("./public"));
+app.use("/static", express.static("./static"));
 
 app.use(routes);
 
